f2006fb250d9596104582242e5c7a6f67312f396
emabimusik <emabi2015@gmail.com>
2018-07-26 00:12:18 +0200

    fourthS commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {CartComponent} from './cart.component';
 import { CartService } from './cartService';
 const routes: Routes = [
   
-  { path: "angulashop/shopApi", redirectTo:'', pathMatch: 'full' },
+  { path: "angulashop/shopApi", redirectTo:"", pathMatch: 'full' },
   { path: "", component: ProductListComponent },
   { path: "detail/:id", component: ProductDetailComponent  },
   { path: "cart", component: CartComponent  }
